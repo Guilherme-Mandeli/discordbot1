315d5bdd0d12d12313e660dbf457e8f581639e18
Guilherme Mandeli <guil.mandeli@gmail.com>
2023-07-04 22:47:46 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
 /*
- * Variables 
+ * Variables
  */
-const { client } = require("./requeriments");
-const { thinkVoiceChannelName, applyVoiceChannelName, nameChanges, channelCache } = require("../manager/voiceChannelManager");
+const { client } = require('./requeriments');
+const {
+	thinkVoiceChannelName,
+	applyVoiceChannelName,
+	nameChanges,
+	channelCache,
+} = require('../manager/voiceChannelManager');
 
 /*
  * Content
  */
 client.on('ready', async () => {
-  nameChanges.clear();
-  channelCache.clear();
+	nameChanges.clear();
+	channelCache.clear();
 
-  console.log(`✅ ${client.user.username} connected!`);
+	console.log(`✅ ${client.user.username} connected!`);
 
-  const guild = client.guilds.cache.get(process.env.GUILD_ID);
-  guild.channels.cache.filter(channel => channel.type === 'voice').forEach(channel => {
-    channelCache.set(channel.id, channel);
-  });
+	const guild = client.guilds.cache.get(process.env.GUILD_ID);
+	guild.channels.cache
+		.filter((channel) => channel.type === 'voice')
+		.forEach((channel) => {
+			channelCache.set(channel.id, channel);
+		});
 
-  setInterval(applyVoiceChannelName, 5000);
-
+	setInterval(applyVoiceChannelName, 5000);
 });
 
-client.on("voiceStateUpdate", async (oldState, newState) => {
-  const newChannel = newState.channel;
-  const oldChannel = oldState.channel;
+client.on('voiceStateUpdate', async (oldState, newState) => {
+	const newChannel = newState.channel;
+	const oldChannel = oldState.channel;
 
-  thinkVoiceChannelName( oldChannel, newChannel );
-
+	thinkVoiceChannelName(oldChannel, newChannel);
 });
 
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
+client.on('messageCreate', (message) => {
+	if (message.author.bot) return;
 
-  if (message.content === "ping") {
-    message.reply("Pong!");
-  }
+	if (message.content === 'ping') {
+		message.reply('Pong!');
+	}
 });
-
 
 client.login(process.env.CLIENT_TOKEN);
